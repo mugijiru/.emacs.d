426f836b92c003d7e8263ccd8b46c43882508007
mugijiru <mugijiru.dev@gmail.com>
2019-04-21 18:16:35 +0900

    ブログ投稿のための諸々を用意
    - seesaa-blog-mode を用意
    - seesaa-blog-mode で使うため emasc-w3m を用意
    - seesaa-blog-mode 用に org の html 出力を調整
      - これはちょっとイケてないのでどこかで直したい

--- init.el
+++ init.el
@@ -11,7 +11,7 @@
     (goto-char (point-max))
     (eval-print-last-sexp)))
 
-(add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
+(add-to-list 'el-get-recipe-path "~/.emacs.d/recipes")
 
 ;; for exec path
 ;; use .bashrc setted path
@@ -40,7 +40,39 @@
 
 ;; for other edit
 (el-get-bundle org-mode) ;; from Git. because melpa cannot resolve dependencies.
+;; フッターなくしたり
+(setq org-export-html-footnotes-section "")
+(setq org-export-html-footnote-format "")
+(setq org-export-with-footnotes nil)
+(setq org-export-with-creator nil)
+(setq org-export-with-author nil)
+(setq org-html-validation-link nil)
 
+;; for seesaa blog settings
+(setq org-export-author-info nil)
+(setq org-export-email-info nil)
+(setq org-export-creator-info nil)
+(setq org-export-time-stamp-file nil)
+(setq org-export-with-timestamps nil)
+(setq org-export-with-section-numbers nil)
+(setq org-export-with-sub-superscripts nil)
+
+;;; Table of Contents を出さない
+(setq org-export-with-toc nil)
+
+;;; h1でサイト名出さない
+(setq org-export-html-preamble nil)
+(setq org-html-preamble nil)
+
+;;; *bold* とか /italic/ とか _underline_ とかを<b>とかにしないようにする
+(setq org-export-with-emphasize nil)
+
+;; デフォは日本語設定
+(setq org-export-default-language "ja")
+
+(el-get-bundle seesaa-blog-mode) ;; for seesaa blog. dependent org-mdoe
+
+(el-get-bundle emacs-w3m)
 
 (el-get-bundle helm)
 (require 'helm-config)
