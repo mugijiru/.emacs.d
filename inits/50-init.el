7c79c4608a8d6cf8b46cb77c3fcfd46deea44414
mugijiru <106833+mugijiru@users.noreply.github.com>
2019-04-24 16:12:07 +0900

    グローバルなキーバインド設定を移動

--- inits/50-init.el
+++ inits/50-init.el
@@ -142,37 +142,3 @@
 (setq migemo-options '("-q" "--emacs"))
 (setq migemo-coding-system 'utf-8-unix)
 (migemo-init)
-
-;; keybinds
-
-(if (eq window-system 'ns)
-    (progn
-          (setq ns-alternate-modifier (quote super)) ;; option  => super
-          (setq ns-command-modifier (quote meta))))  ;; command => meta
-
-;; C-h を backspace に
-(keyboard-translate ?\C-h ?\C-?)
-(global-set-key "\C-h" nil)
-
-;; M-g rをstring-replaceに割り当て
-(global-set-key (kbd "M-g r") 'replace-string)
-
-;; Shift+矢印でwindow移動
-(windmove-default-keybindings)
-
-;; ¥ ではなく \ になるように調整
-(define-key global-map [?¥] [?\\])
-(define-key global-map [?\C-¥] [?\C-\\])
-(define-key global-map [?\M-¥] [?\M-\\])
-(define-key global-map [?\C-\M-¥] [?\C-\M-\\])
-
-;; multiple-cursors
-(global-set-key (kbd "C-:") 'mc/edit-lines)
-(global-set-key (kbd "C->") 'mc/mark-next-like-this)
-(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
-(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
-
-;; helm
-(global-set-key (kbd "M-y") 'helm-show-kill-ring)
-(global-set-key (kbd "C-;") 'helm-for-files)
-(global-set-key (kbd "M-x") 'helm-M-x)
