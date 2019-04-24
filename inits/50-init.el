b2d9cda660cd4d4f20f669fc2f5b6334c1bef942
mugijiru <mugijiru.dev@gmail.com>
2019-04-24 16:38:12 +0900

    ddskk の設定も分離

--- inits/50-init.el
+++ inits/50-init.el
@@ -110,20 +110,3 @@
 
 (el-get-bundle emacs-w3m)
 (el-get-bundle scratch-log)
-
-
-;; for japanese
-(el-get-bundle ddskk)
-(add-hook 'skk-load-hook
-          (lambda ()
-            ;; コード中では自動的に英字にする。
-            (require 'context-skk)
-
-            ;; 半角で入力したい文字
-            (setq skk-rom-kana-rule-list
-                  (nconc skk-rom-kana-rule-list
-                         '((";" nil nil)
-                           (":" nil nil)
-                           ("?" nil nil)
-                           ("!" nil nil))))))
-(setq skk-sticky-key ";")
