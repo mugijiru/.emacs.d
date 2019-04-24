2c3a23cc24bbb5f1dc54cb4266404bd37e5c29ce
mugijiru <mugijiru.dev@gmail.com>
2019-04-24 16:00:21 +0900

    custom-file の設定を切り出し

--- inits/50-init.el
+++ inits/50-init.el
@@ -207,6 +207,3 @@
 (el-get-bundle molokai-theme)
 (add-to-list 'custom-theme-load-path (expand-file-name "~/.emacs.d/el-get/molokai-theme"))
 (load-theme 'molokai t)
-
-(setq custom-file (expand-file-name "~/.emacs.d/custom.el"))
-(load custom-file)
