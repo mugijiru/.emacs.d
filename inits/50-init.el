81a859ddbb4982803d66d3415da10b4efc1fe9ea
mugijiru <106833+mugijiru@users.noreply.github.com>
2019-04-24 16:07:08 +0900

    theme 読み込みを分離

--- inits/50-init.el
+++ inits/50-init.el
@@ -203,7 +203,3 @@
     (add-hook 'window-setup-hook
                 (lambda ()
                   (set-frame-parameter nil 'fullscreen 'fullboth))))
-
-(el-get-bundle molokai-theme)
-(add-to-list 'custom-theme-load-path (expand-file-name "~/.emacs.d/el-get/molokai-theme"))
-(load-theme 'molokai t)
