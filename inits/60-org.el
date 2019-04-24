2259eccb6c01daae2c22abb60f506282a3b00302
mugijiru <mugijiru.dev@gmail.com>
2019-04-24 18:20:15 +0900

    org-mode の設定を分離

---
+++ inits/60-org.el
@@ -0,0 +1,31 @@
+;; for other edit
+(el-get-bundle org-mode) ;; from Git. because melpa cannot resolve dependencies.
+;; フッターなくしたり
+(setq org-export-html-footnotes-section "")
+(setq org-export-html-footnote-format "")
+(setq org-export-with-footnotes nil)
+(setq org-export-with-creator nil)
+(setq org-export-with-author nil)
+(setq org-html-validation-link nil)
+
+;; for seesaa blog settings
+(setq org-export-author-info nil)
+(setq org-export-email-info nil)
+(setq org-export-creator-info nil)
+(setq org-export-time-stamp-file nil)
+(setq org-export-with-timestamps nil)
+(setq org-export-with-section-numbers nil)
+(setq org-export-with-sub-superscripts nil)
+
+;;; Table of Contents を出さない
+(setq org-export-with-toc nil)
+
+;;; h1でサイト名出さない
+(setq org-export-html-preamble nil)
+(setq org-html-preamble nil)
+
+;;; *bold* とか /italic/ とか _underline_ とかを<b>とかにしないようにする
+(setq org-export-with-emphasize nil)
+
+;; デフォは日本語設定
+(setq org-export-default-language "ja")
