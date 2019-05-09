a227c9eaf7c1643475874611bd5eaf58a4a7c582
mugijiru <106833+mugijiru@users.noreply.github.com>
2019-05-09 12:51:21 +0900

    projectile の設定を追加した。あまりうまくいってない気もするが

--- inits/30-projectile.el
+++ inits/30-projectile.el
@@ -1,3 +1,20 @@
 (el-get-bundle projectile)
+
+;; 無視するディレクトリ
+(add-to-list 'projectile-globally-ignored-directories "tmp")
+(add-to-list 'projectile-globally-ignored-directories ".tmp")
+(add-to-list 'projectile-globally-ignored-directories "vendor")
+(add-to-list 'projectile-globally-ignored-directories ".sass-cache")
+(add-to-list 'projectile-globally-ignored-directories "coverage")
+(add-to-list 'projectile-globally-ignored-directories "cache")
+(add-to-list 'projectile-globally-ignored-directories "log")
+
+;; 無視するファイル
+(add-to-list 'projectile-globally-ignored-files "gems.tags")
+(add-to-list 'projectile-globally-ignored-files "project.tags")
+(add-to-list 'projectile-globally-ignored-files "manifest.json")
+
 (el-get-bundle helm-projectile)
 (helm-projectile-on)
+
+(projectile-mode)
