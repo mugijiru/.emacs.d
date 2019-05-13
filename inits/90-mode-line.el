c7a29b6fe897398453a7166d657d87f8a319998b
mugijiru <106833+mugijiru@users.noreply.github.com>
2019-05-13 18:27:48 +0900

    modeline のアイコン設定をさらに追加

--- inits/90-mode-line.el
+++ inits/90-mode-line.el
@@ -13,11 +13,18 @@
 ;; minor-mode
 (my/diminish "helm" 'helm-mode " ^")
 (my/diminish "git-gutter" 'git-gutter-mode " GG")
+(my/diminish "paredit" 'paredit-mode " Pe")
 (my/diminish "yasnippet" 'yas-minor-mode " Ys")
 (my/diminish "whitespace" 'whitespace-mode "◽")
 (my/diminish "whitespace" 'global-whitespace-mode "◽")
 
 ;; major-mode
-(add-hook 'emacs-lisp-mode-hook (lambda () (setq mode-name "λ")))
+(add-hook 'emacs-lisp-mode-hook (lambda () (setq mode-name (all-the-icons-fileicon "elisp"))))
 (add-hook 'enh-ruby-mode-hook (lambda () (setq mode-name "e💎")))
 (add-hook 'ruby-mode-hook (lambda () (setq mode-name "💎")))
+(add-hook 'vue-mode-hook (lambda ()
+                           (make-local-variable 'mmm-buffer-mode-display-name)
+                           (setq mmm-buffer-mode-display-name (all-the-icons-fileicon "vue"))))
+(add-hook 'js-mode-hook (lambda () (setq mode-name (all-the-icons-alltheicon "javascript-badge"))))
+(add-hook 'pug-mode-hook (lambda () (setq mode-name (all-the-icons-fileicon "pug"))))
+(add-hook 'css-mode-hook (lambda () (setq mode-name (all-the-icons-faicon "css3"))))
