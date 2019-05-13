d890a6f937339e181e5dc330531b0c540317c456
mugijiru <mugijiru.dev@gmail.com>
2019-05-13 18:53:28 +0900

    vue ファイルで auto-insert が動くようにした

--- custom.el
+++ custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(auto-insert-directory "~/.emacs.d/insert/")
  '(column-number-mode t)
  '(css-indent-offset 2)
  '(custom-safe-themes
