fd55e6fb21ffcb8ffddaacba236ccd8827ea1e8e
mugijiru <106833+mugijiru@users.noreply.github.com>
2019-05-13 17:19:53 +0900

    mode-line をカスタマイズし始めた

---
+++ inits/90-mode-line.el
@@ -0,0 +1,19 @@
+(el-get-bundle diminish)
+(require 'diminish)
+
+(defmacro my/diminish (file mode &optional new-name)
+  "https://github.com/larstvei/dot-emacs/blob/master/init.org"
+  `(with-eval-after-load ,file
+     (diminish ,mode ,new-name)))
+
+;; minor-mode
+(my/diminish "helm" 'helm-mode " ^")
+(my/diminish "git-gutter" 'git-gutter-mode " GG")
+(my/diminish "yasnippet" 'yas-minor-mode " Ys")
+(my/diminish "whitespace" 'whitespace-mode "◽")
+(my/diminish "whitespace" 'global-whitespace-mode "◽")
+
+;; major-mode
+(add-hook 'emacs-lisp-mode-hook (lambda () (setq mode-name "λ")))
+(add-hook 'enh-ruby-mode-hook (lambda () (setq mode-name "e💎")))
+(add-hook 'ruby-mode-hook (lambda () (setq mode-name "💎")))
