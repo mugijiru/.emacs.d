2b76f603dba88e0d2542ef358c2a33acc1547caf
mugijiru <106833+mugijiru@users.noreply.github.com>
2019-05-14 18:46:14 +0900

    enh-ruby-mode の設定を少し追加

--- inits/40-ruby.el
+++ inits/40-ruby.el
@@ -1,10 +1,12 @@
 (el-get-bundle rbenv)
 (global-rbenv-mode)
 (el-get-bundle enh-ruby-mode)
-(el-get-bundle ruby-end)
+(el-get-bundle ruby-end) ;; end の自動挿入とハイライト
 
 (with-eval-after-load 'enh-ruby-mode
-  (setq enh-ruby-add-encoding-comment-on-save nil))
+  (setq enh-ruby-add-encoding-comment-on-save nil)
+  (setq enh-ruby-deep-indent-paren nil)
+  (setq enh-ruby-bounce-deep-indent t))
 
 (defun my/enh-ruby-mode-hook ()
   (display-line-numbers-mode 1))
