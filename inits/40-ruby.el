50cab4ea64469f8d21ad2e637e38e90e2806b0a9
mugijiru <106833+mugijiru@users.noreply.github.com>
2019-05-14 18:41:52 +0900

    ruby の場合は行番号を表示するようにした

--- inits/40-ruby.el
+++ inits/40-ruby.el
@@ -4,3 +4,8 @@
 
 (with-eval-after-load 'enh-ruby-mode
   (setq enh-ruby-add-encoding-comment-on-save nil))
+
+(defun my/enh-ruby-mode-hook ()
+  (display-line-numbers-mode 1))
+
+(add-hook 'enh-ruby-mode-hook 'my/enh-ruby-mode-hook)
