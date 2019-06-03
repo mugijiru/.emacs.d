a3d987017d54d914e472af4c0e1f3f86e7f0810d
mugijiru <mugijiru.dev@gmail.com>
2019-06-03 09:00:46 +0900

    GitHub と連携できる forge を使えるようにした

--- custom.el
+++ custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(auth-sources (quote ("~/.emacs.d/.authinfo.gpg")))
  '(auto-insert-directory "~/.emacs.d/insert/")
  '(column-number-mode t)
  '(css-indent-offset 2)
