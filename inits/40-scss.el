d1832cdc2a8bb50de7bea4d6343bc1a9552470d4
mugijiru <mugijiru.dev@gmail.com>
2019-06-11 14:40:30 +0900

    scss で自動的に company と flycheck が有効になるようにした

--- inits/40-scss.el
+++ inits/40-scss.el
@@ -2,9 +2,11 @@
 (with-eval-after-load 'scss-mode
   (setq css-indent-offset 2))
 (defun my/scss-mode-hook ()
-  (flycheck-mode)
+  (flycheck-mode 1)
   (make-local-variable 'flycheck-checker)
   (setq flycheck-checker 'scss-stylelint)
+
+  (company-mode 1)
 
   (rainbow-mode))
 (add-hook 'scss-mode-hook 'my/scss-mode-hook)
