bb4ed43c05781b1f5cd9fdc2a00e911377d27476
mugijiru <mugijiru.dev@gmail.com>
2019-06-11 14:39:48 +0900

    es6 で自動的に flycheck と company が有効になるようにした

--- inits/40-es6.el
+++ inits/40-es6.el
@@ -1,7 +1,10 @@
 (el-get-bundle js2-mode)
 (defun my/js2-mode-hook ()
-  (flycheck-mode)
+  (flycheck-mode 1)
   (setq flycheck-disabled-checkers '(javascript-standard javascript-jshint))
+
+  (company-mode 1)
+
   (setq js2-basic-offset 2))
 (add-to-list 'auto-mode-alist '("\\.es6$" . js2-mode))
 (add-hook 'js2-mode-hook 'my/js2-mode-hook)
