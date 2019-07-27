ae4f8bf93c471610817008d4a0d04564157fd5e7
mugijiru <mugijiru.dev@gmail.com>
2019-07-27 22:05:17 +0900

    hydra 経由で dumb-jump を使うようにした

--- inits/81-hydra.el
+++ inits/81-hydra.el
@@ -1,13 +1,23 @@
 (el-get-bundle hydra)
 
+(defhydra dumb-jump-hydra (:color blue :columns 3)
+    "Dumb Jump"
+    ("j" dumb-jump-go "Go")
+    ("o" dumb-jump-go-other-window "Other window")
+    ("e" dumb-jump-go-prefer-external "Go external")
+    ("x" dumb-jump-go-prefer-external-other-window "Go external other window")
+    ("i" dumb-jump-go-prompt "Prompt")
+    ("l" dumb-jump-quick-look "Quick look")
+    ("b" dumb-jump-back "Back"))
+
 (defhydra hydra-usefull-commands (:color teal)
   "
-     ^File^                ^Edit^              ^Search^      ^Other^
+     ^File^                ^Edit^              ^Search^        ^Other^
 ------------------------------------------------------------------------------------------
-  _p_: switch-project    _v_: repalce var      _g_: grep     _c_: calendar
-  _r_: recent file       _a_: align regexp     ^ ^           _P_: open-requested-pr
-  _d_: dir               _+_: 文字拡大         ^ ^           _D_: delete-other-windows
-  _f_: find file         _-_: 文字縮小         ^ ^           _q_: cancel
+  _p_: switch-project    _v_: repalce var      _g_: grep       _c_: calendar
+  _r_: recent file       _a_: align regexp     _j_: dumb jump  _P_: open-requested-pr
+  _d_: dir               _+_: 文字拡大         ^ ^             _D_: delete-other-windows
+  _f_: find file         _-_: 文字縮小         ^ ^             _q_: cancel
 
 "
   ("p" helm-projectile-switch-project)
@@ -21,6 +31,7 @@
   ("-" text-scale-decrease)
 
   ("g" helm-projectile-ag)
+  ("j" dumb-jump-hydra/body)
 
   ("c" my/open-user-calendar)
   ("P" my/open-review-requested-pr)
