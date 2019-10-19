0a80d1184cbf68db382ecd8529ad956907b11022
mugijiru <mugijiru.dev@gmail.com>
2019-10-19 14:56:12 +0900

    byebug などを使えるように調整

--- inits/42-rspec.el
+++ inits/42-rspec.el
@@ -1,1 +1,7 @@
 (el-get-bundle rspec-mode)
+
+;; rspec 実行バッファで byebug などで止った際に
+;; C-x C-q したら inf-ruby が動くようにする
+;; byebug か binding.irb 推奨。
+;; binding.pry は何故かまともに動かない
+(add-hook 'after-init-hook 'inf-ruby-switch-setup)
