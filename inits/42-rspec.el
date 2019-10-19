8b021d895f6d49fa08d7aa7754dc2441f2ffd234
mugijiru <mugijiru.dev@gmail.com>
2019-10-19 15:20:46 +0900

    現在の行を C-c C-c でテストできるようにした

--- inits/42-rspec.el
+++ inits/42-rspec.el
@@ -5,3 +5,6 @@
 ;; byebug か binding.irb 推奨。
 ;; binding.pry は何故かまともに動かない
 (add-hook 'after-init-hook 'inf-ruby-switch-setup)
+
+;; C-c C-c で現在の行のテストを実行
+(define-key rspec-mode-map (kbd "C-c C-c") 'rspec-verify-single)
