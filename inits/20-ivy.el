eb1b9b97d586574b69e839bc65497bac9402a80b
mugijiru <mugijiru.dev@gmail.com>
2019-10-21 08:41:09 +0900

    公式ドキュメントに従った形で icon を表示できるようにした
    switch-buffer だけ対応したけど多分他のやつでもできそう?

--- inits/20-ivy.el
+++ inits/20-ivy.el
@@ -29,3 +29,25 @@
 
 (el-get-bundle Yevgnen/ivy-rich)
 (ivy-rich-mode 1)
+
+;; https://github.com/Yevgnen/ivy-rich#1312-add-icons-for-ivy-switch-buffer
+(defun ivy-rich-switch-buffer-icon (candidate)
+  (with-current-buffer
+      (get-buffer candidate)
+    (let ((icon (all-the-icons-icon-for-mode major-mode)))
+      (if (symbolp icon)
+          (all-the-icons-icon-for-mode 'fundamental-mode)
+        icon))))
+
+(setq ivy-rich--display-transformers-list
+      '(ivy-switch-buffer
+        (:columns
+         ((ivy-rich-switch-buffer-icon :width 2)
+          (ivy-rich-candidate (:width 30))
+          (ivy-rich-switch-buffer-size (:width 7))
+          (ivy-rich-switch-buffer-indicators (:width 4 :face error :align right))
+          (ivy-rich-switch-buffer-major-mode (:width 12 :face warning))
+          (ivy-rich-switch-buffer-project (:width 15 :face success))
+          (ivy-rich-switch-buffer-path (:width (lambda (x) (ivy-rich-switch-buffer-shorten-path x (ivy-rich-minibuffer-width 0.3))))))
+         :predicate
+         (lambda (cand) (get-buffer cand)))))
