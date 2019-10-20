ba412d747a6b126d3191c13ca582965f508d0da1
mugijiru <mugijiru.dev@gmail.com>
2019-10-21 08:08:40 +0900

    ivy を入れて基本的な設定だけひとまず導入

---
+++ inits/20-ivy.el
@@ -0,0 +1,18 @@
+(el-get-bundle abo-abo/swiper) ;; ivy, swiper, counsel が同時に入って来る
+
+(when (require 'ivy nil t)
+  ;; M-o を ivy-dispatching-done-hydra に割り当てる．
+  (require 'ivy-hydra)
+
+  ;; `ivy-switch-buffer' (C-x b) のリストに recent files と bookmark を含める．
+  (setq ivy-use-virtual-buffers t)
+
+  ;; ミニバッファでコマンド発行を認める
+  (when (setq enable-recursive-minibuffers t)
+    (minibuffer-depth-indicate-mode 1)) ;; 何回層入ったかプロンプトに表示．
+
+  ;; ESC連打でミニバッファを閉じる
+  (define-key ivy-minibuffer-map (kbd "<escape>") 'minibuffer-keyboard-quit)
+
+  ;; アクティベート
+  (ivy-mode 1))
