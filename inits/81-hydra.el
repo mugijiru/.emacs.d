33cd25ccfa319c5f7b60651a64a62503aa7b8864
mugijiru <106833+mugijiru@users.noreply.github.com>
2019-10-26 23:14:04 +0900

    google-this 向け pretty-hydra 設定を移動

--- inits/81-hydra.el
+++ inits/81-hydra.el
@@ -42,24 +42,6 @@
    "Other"
    (("b" dumb-jump-back "Back"))))
 
-(pretty-hydra-define google-this-pretty-hydra
-  (:foreign-keys warn :title "Google" :quit-key "q" :color blue :separator "-")
-  ("Current"
-   (("SPC" google-this-noconfirm "No Confirm")
-    ("RET" google-this           "Auto")
-    ("w"   google-this-word      "Word")
-    ("l"   google-this-line      "Line")
-    ("s"   google-this-symbol    "Symbol")
-    ("r"   google-this-region    "Region")
-    ("e"   google-this-error     "Error"))
-
-   "Feeling Lucky"
-   (("L"   google-this-lucky-search         "Lucky")
-    ("i"   google-this-lucky-and-insert-url "Insert URL"))
-
-   "Tool"
-   (("W" google-this-forecast "Weather"))))
-
 (pretty-hydra-define pretty-hydra-usefull-commands (:separator "-" :color teal :foreign-key warn :title "Usefull commands" :quit-key "q")
   ("File"
    (("p" counsel-projectile-switch-project "Switch Project")
