56d48c8dea7b6ffff126e03c9daa01c673bc07ee
mugijiru <mugijiru.dev@gmail.com>
2019-11-02 19:39:43 +0900

    pretty-hydra を上書きしてとりあえず目的を達成
    最後の行が表示されない問題をこれで解決した。

--- inits/81-hydra.el
+++ inits/81-hydra.el
@@ -9,6 +9,22 @@
 (el-get-bundle pretty-hydra)
 (el-get-bundle major-mode-hydra)
 
+;; override
+;; posframe が最初に空行があると最後お行を表示しないため
+;; 一時的にこちらを直してみている
+(with-eval-after-load 'pretty-hydra
+  (defun pretty-hydra--maybe-add-title (title docstring)
+  "Add TITLE to the DOCSTRING if it's not nil, other return DOCSTRING unchanged."
+  (if (null title)
+      docstring
+    (format "%s\n%s"
+            (cond
+             ((char-or-string-p title) title)
+             ((symbolp title)          (format "%%s`%s" title))
+             ((listp title)            (format "%%s%s" (prin1-to-string title)))
+             (t                        ""))
+            docstring))))
+
 (pretty-hydra-define pretty-hydra-usefull-commands (:separator "-" :color teal :foreign-key warn :title "Usefull commands" :quit-key "q")
   ("File"
    (("p" counsel-projectile-switch-project "Switch Project")
