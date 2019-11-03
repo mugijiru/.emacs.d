f667f53df468fca5db5c3de824f36b75caf22a47
mugijiru <mugijiru.dev@gmail.com>
2019-11-03 15:54:32 +0900

    scss-mode で stylelint が動いてなかったので修正した
    lsp-mode とかに設定を上書きされていたのが原因の模様

--- inits/40-scss.el
+++ inits/40-scss.el
@@ -3,11 +3,12 @@
   (setq css-indent-offset 2))
 (defun my/scss-mode-hook ()
   (flycheck-mode 1)
-  (make-local-variable 'flycheck-checker)
-  (setq flycheck-checker 'scss-stylelint)
-
   (lsp)
   (lsp-ui-mode -1) ;; lsp-ui が有効だと画面上でガチャガチャ height とかの説明をしてうざいので
+
+  ;; lsp-ui とかより後に設定しないと上書きされるのでここに移動した
+  (make-local-variable 'flycheck-checker)
+  (setq flycheck-checker 'scss-stylelint)
 
   (company-mode 1)
   (display-line-numbers-mode 1)
