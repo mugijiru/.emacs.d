ccb4710363bb288a0f40b7ee7f9ac54f06613bdf
mugijiru <106833+mugijiru@users.noreply.github.com>
2020-05-09 14:00:00 +0900

    dashboard を自分用に少々カスタマイズ

--- inits/92-dashboard.el
+++ inits/92-dashboard.el
@@ -1,2 +1,19 @@
 (el-get-bundle dashboard)
+
+;; 表示するアイコンをロゴに変更
+(setq dashboard-startup-banner 'logo)
+
+;; 表示する情報の設定
+(setq dashboard-items '((recents  . 5)
+                        ;; (bookmarks . 5) ;; bookmarks は使ってない
+                        (projects . 5)
+                        (agenda . 5)
+                        ;; (registers . 5) ;; registers は使ってない
+                        ))
+
+;; 各セクションのタイトル部の先頭にアイコンを表示
+(setq dashboard-set-heading-icons t)
+
+;; 各ファイルの先頭にアイコンを表示
+(setq dashboard-set-file-icons t)
 (dashboard-setup-startup-hook)
