f49d90b4d64ac41a6642a456c3e90c122a94160d
mugijiru <106833+mugijiru@users.noreply.github.com>
2020-05-17 19:57:11 +0900

    org-trello-mode が有効になったら org-trello 用の設定を読むように
    しかしこれだと普通の org-mode の時にも有効になるので
    別の手段を考えた方が良さそう

--- inits/61-org-trello.el
+++ inits/61-org-trello.el
@@ -1,1 +1,15 @@
 (el-get-bundle org-trello)
+(defun my/org-trello-fetch-buffer ()
+  (org-trello-sync-buffer t))
+
+(defun my/org-trello-mode-hook ()
+  (major-mode-hydra-define+ org-mode (:quit-key "q" :title (concat (all-the-icons-faicon "trello") " Org & trello"))
+    ("Hoge"
+     (("i" org-trello-install-board-metadata "Board metadata")
+      ("I" org-trello-install-key-and-token "Setup")
+
+      ("V" org-trello-version "Version")
+      ("s" my/org-trello-fetch-buffer "← Trello")
+      ("S" org-trello-sync-buffer "→ Trello")))))
+
+(add-hook 'org-trello-mode-hook 'my/org-trello-mode-hook)
