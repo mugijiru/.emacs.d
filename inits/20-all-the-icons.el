e0d5d6c4d7d8b3366634d5a348e29206b4f6ee0a
mugijiru <106833+mugijiru@users.noreply.github.com>
2020-05-30 11:28:55 +0900

    余分な空白の削除

--- inits/20-all-the-icons.el
+++ inits/20-all-the-icons.el
@@ -5,10 +5,10 @@
 (with-eval-after-load 'pretty-hydra
   (pretty-hydra-define all-the-icons-hydra (:separator "-" :title "All the icons" :exit t :quit-key "q")
     ("Insert"
-     (("a" all-the-icons-insert-alltheicon  "All the icons")
-      ("f" all-the-icons-insert-fileicon    "File icons")
-      ("F" all-the-icons-insert-faicons     "FontAwesome")
-      ("m" all-the-icons-insert-material    "Material")
-      ("o" all-the-icons-insert-octicon     "Octicon")
-      ("w" all-the-icons-insert-wicon       "Weather")
-      ("*" all-the-icons-insert             "All")))))
+     (("a" all-the-icons-insert-alltheicon "All the icons")
+      ("f" all-the-icons-insert-fileicon   "File icons")
+      ("F" all-the-icons-insert-faicons    "FontAwesome")
+      ("m" all-the-icons-insert-material   "Material")
+      ("o" all-the-icons-insert-octicon    "Octicon")
+      ("w" all-the-icons-insert-wicon      "Weather")
+      ("*" all-the-icons-insert            "All")))))
