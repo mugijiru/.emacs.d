3ff81e353f2ca2729906654d2b900a650d4feaf2
mugijiru <106833+mugijiru@users.noreply.github.com>
2020-06-28 20:35:53 +0900

    まず MELPA で入ってる gnuplot を削除

--- custom.el
+++ custom.el
@@ -37,7 +37,7 @@
    (expand-file-name "~/.emacs.d/el-get/plantuml-mode/plantuml.jar"))
  '(package-selected-packages
    (quote
-    (gnu-elpa-keyring-update gnuplot oauth2 highlight-indent-guides rainbow-mode molokai-theme xml-rpc)))
+    (gnu-elpa-keyring-update oauth2 highlight-indent-guides rainbow-mode molokai-theme xml-rpc)))
  '(projectile-mode-line-prefix "")
  '(pug-tab-width 2)
  '(recentf-auto-cleanup (quote never))
