4c7b1b8afb1e181c2bf128c6eab2fdaad975a43e
mugijiru <106833+mugijiru@users.noreply.github.com>
2020-07-11 12:51:10 +0900

    persist を入れる設定のコミット漏れ対応

--- custom.el
+++ custom.el
@@ -37,7 +37,7 @@
    (expand-file-name "~/.emacs.d/el-get/plantuml-mode/plantuml.jar"))
  '(package-selected-packages
    (quote
-    (gnu-elpa-keyring-update oauth2 highlight-indent-guides rainbow-mode xml-rpc)))
+    (persist gnu-elpa-keyring-update oauth2 highlight-indent-guides rainbow-mode xml-rpc)))
  '(projectile-mode-line-prefix "")
  '(pug-tab-width 2)
  '(recentf-auto-cleanup (quote never))
