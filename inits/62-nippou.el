96b409dabe6114459cb250a916de366d13206574
mugijiru <106833+mugijiru@users.noreply.github.com>
2020-07-21 23:33:53 +0900

    日報生成に org-ql を使うようにした

---
+++ inits/62-nippou.el
@@ -0,0 +1,19 @@
+(load "my-nippou-config")
+(defun my/org-nippou-targets ()
+      (-concat (my/org-nippou-files) my/org-nippou-additional-files))
+
+(defun my/org-nippou-files ()
+  (let* ((dir my/org-tasks-directory)
+         (cmd (format "find \"%s\" -name '*.org' -or -name '*.org_archive'" dir))
+         (result (shell-command-to-string cmd))
+         (file-names (split-string result "\n")))
+    (-remove (lambda (file-name) (string= "" file-name))
+             file-names)))
+
+(defun my/nippou-query ()
+  (interactive)
+  (org-ql-search
+    (my/org-nippou-targets)
+    "todo:TODO,DOING,WAIT,DONE ts:on=today"
+    :title "日報"
+    :super-groups '((:auto-group))))
