cc3a9bd843c850c658cb09f24eb53538312a81c7
mugijiru <106833+mugijiru@users.noreply.github.com>
2020-08-02 00:09:26 +0900

    Ubuntu でも migemo が使えるように設定を修正

---
+++ inits/01-load-libraries.el
@@ -0,0 +1,2 @@
+(el-get-bundle s)
+(require 's)
--- inits/10-migemo.el
+++ inits/10-migemo.el
@@ -1,7 +1,21 @@
 (el-get-bundle migemo)
 (load "migemo")
-(setq migemo-dictionary "/usr/local/share/migemo/utf-8/migemo-dict")
-(setq migemo-command "/usr/local/bin/cmigemo")
+
+;; Mac
+(let ((path "/usr/local/share/migemo/utf-8/migemo-dict"))
+  (if (file-exists-p path)
+      (setq migemo-dictionary path)))
+
+;; Ubuntu
+(let ((path "/usr/share/cmigemo/utf-8/migemo-dict"))
+  (if (file-exists-p path)
+      (setq migemo-dictionary path)))
+
+(let ((path (s-chomp (shell-command-to-string "which cmigemo"))))
+  (if (s-ends-with? "not found" path)
+      (message "cmigemo not found")
+    (setq migemo-command path)))
+
 (setq migemo-options '("-q" "--emacs"))
 (setq migemo-coding-system 'utf-8-unix)
 (migemo-init)
