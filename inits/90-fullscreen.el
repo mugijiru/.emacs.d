e56b3b7d3a64c1b155c7565253f8aa95773fe210
mugijiru <106833+mugijiru@users.noreply.github.com>
2020-08-02 11:02:21 +0900

    X で fullscreen で左上に寄るようにした

--- inits/90-fullscreen.el
+++ inits/90-fullscreen.el
@@ -2,3 +2,9 @@
     (add-hook 'window-setup-hook
               (lambda ()
                 (set-frame-parameter nil 'fullscreen 'fullboth))))
+
+(if (eq window-system 'x)
+    (add-hook 'window-setup-hook
+              (lambda ()
+                (set-frame-parameter nil 'fullscreen 'fullboth)
+                (set-frame-position nil 0 0))))
