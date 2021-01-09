ff24352bacee7fa62135e4389180732c50fc40e5
mugijiru <106833+mugijiru@users.noreply.github.com>
2021-01-09 19:01:22 +0900

    hydra から frame size を変更できるようにした

--- inits/80-frame-cmds.el
+++ inits/80-frame-cmds.el
@@ -5,4 +5,10 @@
    (("h" move-frame-left  "Left")
     ("j" move-frame-down  "Down")
     ("k" move-frame-up    "Up")
-    ("l" move-frame-right "Right"))))
+    ("l" move-frame-right "Right"))
+
+   "Resize"
+   (("H" shrink-frame-horizontally "H-")
+    ("J" enlarge-frame "V+")
+    ("K" shrink-frame "V-")
+    ("L" enlarge-frame-horizontally "H+"))))
