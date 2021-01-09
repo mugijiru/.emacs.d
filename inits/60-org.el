65098aa2b2ed377a03adbff1dc5d28f65e4d72dd
mugijiru <106833+mugijiru@users.noreply.github.com>
2021-01-09 18:20:42 +0900

    当日作業用のファイルを統合したので refile 設定を修正
    - 当日作業ツリーは today suffix をつけてるので指定できるようにした
    - カテゴリ設定されてるツリーは C- prefix を付けたので
      それを指定できるようにした
    - それらに伴い失くなった移動先の指定を削除
    - もうやらないだろうという作業は someday に移動できるようにした

--- inits/60-org.el
+++ inits/60-org.el
@@ -223,10 +223,10 @@
 (setq org-refile-targets `((,(concat org-directory "tasks/projects.org") :level . 2)
                            (,(concat org-directory "tasks/pointers.org") :level . 1)
                            (,(concat org-directory "work/scrum/impediments.org") :level . 3)
-                           (,(concat org-directory "tasks/next-actions.org") :level . 1)
+                           (,(concat org-directory "tasks/next-actions.org") :regexp . "today")
+                           (,(concat org-directory "tasks/next-actions.org") :regexp . "C-")
                            (,(concat org-directory "private/2020_summary.org") :level . 2)
-                           (,(concat org-directory "tasks/someday.org") :level . 1)
-                           (,(concat org-directory "tasks/daily-logs.org") :level . 3)))
+                           (,(concat org-directory "tasks/someday.org") :level . 1)))
 
 (defun my/org-tags-view-only-todo ()
   (interactive)
