a7d6786db21abbe8e1b94ac3ecd2eddfc6435905
mugijiru <106833+mugijiru@users.noreply.github.com>
2021-05-16 21:23:37 +0900

    org-gcal の設定を分離

--- inits/60-org.el
+++ inits/60-org.el
@@ -288,10 +288,6 @@
                  :block today
                  :level 4))
 
-(el-get-bundle persist) ;; org-gcal に必要
-(el-get-bundle org-gcal)
-(require 'org-gcal)
-(my/load-config "my-org-gcal-config")
 
 (el-get-bundle ob-async)
 (require 'ob-async)
