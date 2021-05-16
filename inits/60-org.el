0066b81c43694dec450f34bc55adacdc8ed01d2e
mugijiru <106833+mugijiru@users.noreply.github.com>
2021-05-16 21:22:38 +0900

    export の設定を分離した

--- inits/60-org.el
+++ inits/60-org.el
@@ -9,36 +9,6 @@
                                (ruby . t)))
 (setq org-directory (expand-file-name "~/Documents/org/"))
 
-;; フッターなくしたり
-(setq org-export-html-footnotes-section "")
-(setq org-export-html-footnote-format "")
-(setq org-export-with-footnotes nil)
-(setq org-export-with-creator nil)
-(setq org-export-with-author nil)
-(setq org-html-validation-link nil)
-
-;; for seesaa blog settings
-(setq org-export-author-info nil)
-(setq org-export-email-info nil)
-(setq org-export-creator-info nil)
-(setq org-export-time-stamp-file nil)
-(setq org-export-with-timestamps nil)
-(setq org-export-with-section-numbers nil)
-(setq org-export-with-sub-superscripts nil)
-
-;;; Table of Contents を出さない
-(setq org-export-with-toc nil)
-
-;;; h1でサイト名出さない
-(setq org-export-html-preamble nil)
-(setq org-html-preamble nil)
-
-;;; *bold* とか /italic/ とか _underline_ とかを<b>とかにしないようにする
-(setq org-export-with-emphasize nil)
-
-;; デフォは日本語設定
-(setq org-export-default-language "ja")
-
 ;; org-mode のリンク先が xlsx の時に numbers を開くようにした
 ;; default は内部的には open コマンドが使われる
 (add-to-list 'org-file-apps '("\\.xlsx?\\'" . default))
