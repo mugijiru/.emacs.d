90130769cef0a35b69a12047ce8580f7f77571bb
mugijiru <106833+mugijiru@users.noreply.github.com>
2021-05-17 22:31:00 +0900

    package.el から読む必要がないものを読まないようにした

--- custom.el
+++ custom.el
@@ -64,7 +64,7 @@
    (expand-file-name "~/.emacs.d/el-get/plantuml-mode/plantuml.jar"))
  '(package-selected-packages
    (quote
-    (peg persist gnu-elpa-keyring-update oauth2 highlight-indent-guides rainbow-mode xml-rpc)))
+    (peg persist gnu-elpa-keyring-update oauth2 rainbow-mode xml-rpc)))
  '(projectile-mode-line-prefix "")
  '(pug-tab-width 2)
  '(recentf-auto-cleanup (quote never))
