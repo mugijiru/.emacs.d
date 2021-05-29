edb3648fd3208cac364a9586904489643e52f186
mugijiru <106833+mugijiru@users.noreply.github.com>
2021-05-29 13:32:24 +0900

    読み込んだファイルの場所が emacs のフォルダとして使われるようにした

--- init.el
+++ init.el
@@ -1,3 +1,6 @@
+(when load-file-name
+  (setq user-emacs-directory (file-name-directory load-file-name)))
+
 (setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
 			 ("melpa" . "https://melpa.org/packages/")))
 ;; (package-initialize)
