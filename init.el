595cd9c976d5ef13499f0141eff9788967a5dafd
mugijiru <106833+mugijiru@users.noreply.github.com>
2021-06-12 23:01:03 +0900

    leaf-keywords が入る状態にしてみる

--- init.el
+++ init.el
@@ -1,6 +1,23 @@
-(setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
-			 ("melpa" . "https://melpa.org/packages/")))
-;; (package-initialize)
+(setq package-archives '(("org"   . "https://orgmode.org/elpa/")
+                         ("gnu" . "https://elpa.gnu.org/packages/")
+                         ("melpa" . "https://melpa.org/packages/")))
+(package-initialize)
+
+(unless (package-installed-p 'leaf)
+  (package-refresh-contents)
+  (package-install 'leaf))
+
+(leaf leaf-keywords
+      :ensure t
+      :init
+      ;; optional packages if you want to use :hydra, :el-get, :blackout,,,
+      ;; (leaf hydra :ensure t)
+      ;; (leaf el-get :ensure t)
+      (leaf blackout :ensure t)
+
+      :config
+      ;; initialize leaf-keywords.el
+      (leaf-keywords-init))
 
 (load "~/.emacs.d/init-el-get.el")
 
