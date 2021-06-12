7d759352f20f5824a64fc138e22ba5206ba1a060
mugijiru <106833+mugijiru@users.noreply.github.com>
2021-06-12 11:16:06 +0900

    my/org-todo のテストを追加

--- inits/test/68-my-org-commands-test.el
+++ inits/test/68-my-org-commands-test.el
@@ -1,6 +1,12 @@
 (require 'ert)
 
+;; 外部ライブラリの設定
 (load (expand-file-name (concat user-emacs-directory "/init-el-get.el")))
+(load (expand-file-name (concat user-emacs-directory "/inits/99-with-simulated-input")))
+
+(el-get-bundle abo-abo/swiper)
+
+;; テスト対象の読み込み
 (load (expand-file-name (concat user-emacs-directory "/inits/68-my-org-commands.el")))
 
 (ert-deftest test:my/org-todo-keyword-strings ()
@@ -9,3 +15,13 @@
     (should (equal '("TODO" "DOING" "WAIT" "DONE" "SOMEDAY")
                    (my/org-todo-keyword-strings)))))
 
+(ert-deftest test:my/org-todo ()
+  "Test of `my/org-todo'."
+  (let ((org-todo-keywords '((sequence "TODO" "DOING(!)" "WAIT" "|" "DONE(!)" "SOMEDAY(s)")))
+        (result))
+    ;; org-mode を読まずに済むように org-todo を差し替えてテストしている
+    (cl-letf (((symbol-function 'org-todo)
+               (lambda (keyword)
+                 (setq result keyword))))
+      (with-simulated-input "DOI RET" (my/org-todo))
+      (should (equal "DOING" result)))))
