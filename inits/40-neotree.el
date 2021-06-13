a0687b12e8b815d8308ab8a3510ae6bfb378c0a4
mugijiru <106833+mugijiru@users.noreply.github.com>
2021-06-13 20:36:03 +0900

    init.org に色々書いてそれを吐き出すようにした

--- inits/40-neotree.el
+++ inits/40-neotree.el
@@ -1,6 +1,5 @@
 (el-get-bundle emacs-neotree-dev)
-;; counsel-projectile を使ってると意味がないのでコメントアウト
-;; (setq projectile-switch-project-action 'neotree-projectile-action)
+
 (setq neo-theme (if (display-graphic-p) 'icons 'arrow))
 
 (with-eval-after-load 'major-mode-hydra
