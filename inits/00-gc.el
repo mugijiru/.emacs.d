6aca6510bef4341118aa8daeac892ba7edd67a84
mugijiru <106833+mugijiru@users.noreply.github.com>
2021-06-17 22:53:15 +0900

    gcmh の設定を init.org から吐き出すようにした

--- inits/00-gc.el
+++ inits/00-gc.el
@@ -1,10 +1,1 @@
-;; ;; https://gist.github.com/garaemon/8851900ef27d8cb28200ac8d92ebacdf
-;; ;; Increase threshold to fire garbage collection
-;; (setq gc-cons-threshold 1073741824)
-;; (setq garbage-collection-messages t)
-
-
-;; ;; Run GC every 60 seconds if emacs is idle.
-;; (run-with-idle-timer 60.0 t #'garbage-collect)
-
 (el-get-bundle gcmh)
