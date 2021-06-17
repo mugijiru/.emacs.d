112712b612503bbf453fe5ece21a76ebd1788d19
mugijiru <106833+mugijiru@users.noreply.github.com>
2021-06-17 23:02:42 +0900

    ruby-end を削除した
    smartparens と競合するので
    smartparens に任せることにした

--- inits/40-ruby.el
+++ inits/40-ruby.el
@@ -1,7 +1,6 @@
 (el-get-bundle rbenv)
 (global-rbenv-mode)
 (el-get-bundle enh-ruby-mode)
-(el-get-bundle ruby-end) ;; end の自動挿入とハイライト
 
 (with-eval-after-load 'enh-ruby-mode
   (setq enh-ruby-add-encoding-comment-on-save nil)
