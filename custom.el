8940a8cf78dde3a05c90f8adbe844c4d6ef3198f
mugijiru <106833+mugijiru@users.noreply.github.com>
2021-06-20 23:12:18 +0900

    意図しない変更が紛れていたので修正

--- custom.el
+++ custom.el
@@ -4,7 +4,6 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(auth-sources (quote ("~/.emacs.d/.authinfo.gpg")))
- '(auto-insert-directory "~/.emacs.d/insert/")
  '(aw-background t)
  '(column-number-mode t)
  '(counsel-locate-cmd (quote counsel-locate-cmd-mdfind))
