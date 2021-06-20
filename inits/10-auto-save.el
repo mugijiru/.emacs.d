aa5b84d428d8b0b09a63dd8aae409c59436d8d83
mugijiru <106833+mugijiru@users.noreply.github.com>
2021-06-20 15:48:51 +0900

    設定ファイルに反映

--- inits/10-auto-save.el
+++ inits/10-auto-save.el
@@ -1,8 +1,12 @@
 (setq auto-save-timeout 15)
+
 (setq auto-save-interval 60)
+
 (setq auto-save-file-name-transforms '((".*" "~/.emacs.d/backup/" t)))
+
 (setq backup-directory-alist '((".*" . "~/.emacs.d/backup")
                                (,tramp-file-name-regexp . nil)))
 
 (setq version-control t)
+
 (setq delete-old-versions t)
