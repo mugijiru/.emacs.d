6e352aff56f46bc0fcf3483f7b212b6776b0f777
mugijiru <106833+mugijiru@users.noreply.github.com>
2021-06-26 16:58:19 +0900

    google translate の設定を反映

--- inits/20-google-translate.el
+++ inits/20-google-translate.el
@@ -1,1 +1,12 @@
 (el-get-bundle google-translate)
+
+(defun google-translate--search-tkk ()
+  "Search TKK."
+  (list 430675 2721866130))
+
+(require 'google-translate-default-ui)
+
+(custom-set-variables
+  '(google-translate-default-source-language "en")
+  '(google-translate-default-target-language "ja")
+  '(google-translate-output-destination 'popup))
