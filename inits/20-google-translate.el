47d95108d274ffaa360d86b44403ca6fc67155fe
mugijiru <106833+mugijiru@users.noreply.github.com>
2021-06-26 17:24:38 +0900

    そのままだと動かなかったので with-eval-after-load などを使って調整

--- inits/20-google-translate.el
+++ inits/20-google-translate.el
@@ -1,12 +1,14 @@
 (el-get-bundle google-translate)
 
-(defun google-translate--search-tkk ()
-  "Search TKK."
-  (list 430675 2721866130))
+(with-eval-after-load 'google-translate-tk
+  (defun google-translate--search-tkk ()
+    "Search TKK."
+    (list 430675 2721866130)))
 
-(require 'google-translate-default-ui)
+(with-eval-after-load 'popup
+  (require 'google-translate-default-ui))
 
 (custom-set-variables
-  '(google-translate-default-source-language "en")
-  '(google-translate-default-target-language "ja")
-  '(google-translate-output-destination 'popup))
+ '(google-translate-default-source-language "en")
+ '(google-translate-default-target-language "ja")
+ '(google-translate-output-destination 'popup))
