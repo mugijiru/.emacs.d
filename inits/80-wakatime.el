a6744465774b7ec4f0eb07bde35fcab141e7a4f4
mugijiru <106833+mugijiru@users.noreply.github.com>
2021-06-29 23:07:18 +0900

    wakatime の設定ファイルに反映

--- inits/80-wakatime.el
+++ inits/80-wakatime.el
@@ -1,4 +1,6 @@
 (el-get-bundle wakatime-mode)
+
 (custom-set-variables
  '(wakatime-api-key (funcall (plist-get (nth 0 (auth-source-search :host "wakatime.com" :max 1)) :secret))))
+
 (global-wakatime-mode 1)
