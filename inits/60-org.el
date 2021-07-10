f71b49c55e6dfe78e47353b282d11cdfc75e9543
mugijiru <106833+mugijiru@users.noreply.github.com>
2021-07-10 09:57:13 +0900

    設定ファイルに反映

--- inits/60-org.el
+++ inits/60-org.el
@@ -1,5 +1,17 @@
 (el-get-bundle org-mode :checkout "release_9.3.6")
 
+(setq org-directory (expand-file-name "~/Documents/org/"))
+
+(setq my/org-tasks-directory (concat org-directory "tasks/"))
+
+(setq org-todo-keywords
+      '((sequence "TODO" "DOING(!)" "WAIT" "|" "DONE(!)" "SOMEDAY(s)")))
+
+(setq org-log-done 'time)
+(setq org-log-into-drawer "LOGBOOK")
+
+(add-to-list 'org-file-apps '("\\.xlsx?\\'" . default))
+
 (el-get-bundle org-export-blocks-format-plantuml)
 
 (org-babel-do-load-languages 'org-babel-load-languages
@@ -11,24 +23,11 @@
                                (js . t)
                                (ruby . t)))
 
-(setq org-directory (expand-file-name "~/Documents/org/"))
+(add-hook 'org-babel-after-execute-hook 'org-redisplay-inline-images)
 
-(add-to-list 'org-file-apps '("\\.xlsx?\\'" . default))
-
-(setq org-todo-keywords
-      '((sequence "TODO" "DOING(!)" "WAIT" "|" "DONE(!)" "SOMEDAY(s)")))
-
-(setq org-log-done 'time)
-(setq org-log-into-drawer "LOGBOOK")
-
-(setq my/org-tasks-directory (concat org-directory "tasks/"))
-
-;; ob-async
 (el-get-bundle ob-async)
 (require 'ob-async)
 
 (add-hook 'ob-async-pre-execute-src-block-hook
       '(lambda ()
          (setq org-plantuml-jar-path "~/bin/plantuml.jar")))
-
-(add-hook 'org-babel-after-execute-hook 'org-redisplay-inline-images)
