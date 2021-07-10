0a94d5f353c4e39676fefa998770a5c977668987
mugijiru <106833+mugijiru@users.noreply.github.com>
2021-07-11 00:09:10 +0900

    設定ファイルに反映

--- inits/70-key-chord.el
+++ inits/70-key-chord.el
@@ -1,6 +1,6 @@
 (el-get-bundle zk-phi/key-chord)
 
-(setq key-chord-two-keys-delay           0.15
+(setq key-chord-two-keys-delay           0.25
       key-chord-safety-interval-backward 0.1
       key-chord-safety-interval-forward  0.15)
 
