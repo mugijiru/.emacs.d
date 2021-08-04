a6a93729916a230c95ce53662aa5958871d73ee5
mugijiru <106833+mugijiru@users.noreply.github.com>
2021-08-04 17:19:38 +0900

    それ以外の設定ファイルの tangle も反映
    コメントが消えてるだけなので問題なしと判断。
    init.org の方に書いているはずだからね。

--- inits/80-global-keybinds.el
+++ inits/80-global-keybinds.el
@@ -3,30 +3,22 @@
           (setq ns-alternate-modifier (quote super)) ;; option  => super
           (setq ns-command-modifier (quote meta))))  ;; command => meta
 
-;; C-h を backspace に
 (keyboard-translate ?\C-h ?\C-?)
 (global-set-key "\C-h" nil)
 
-;; M-g rをstring-replaceに割り当て
 (global-set-key (kbd "M-g r") 'replace-string)
 
-;; C-\ で skk が有効になるように
 (global-set-key (kbd "C-\\") 'skk-mode)
 
-;; C-s で swiper を代わりに使うことにした
 (global-set-key (kbd "C-s") 'swiper)
 
-;; C-x o で ace-window を起動するようにした
 (global-set-key (kbd "C-x o") 'ace-window)
 
-;; Shift+矢印でwindow移動
 (windmove-default-keybindings)
 
-;; undo-fu
 (global-set-key (kbd "C-/") 'undo-fu-only-undo)
 (global-set-key (kbd "C-M-/") 'undo-fu-only-redo)
 
-;; ¥ ではなく \ になるように調整
 (define-key global-map [?¥] [?\\])
 (define-key global-map [?\C-¥] [?\C-\\])
 (define-key global-map [?\M-¥] [?\M-\\])
@@ -38,32 +30,26 @@
 (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
 (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
 
-;; helm
 ;; (global-set-key (kbd "M-y") 'helm-show-kill-ring)
 (global-set-key (kbd "C-;") 'helm-for-files)
 ;; (global-set-key (kbd "M-x") 'helm-M-x)
 ;; (global-set-key (kbd "C-x b") 'helm-mini)
 ;; (global-set-key (kbd "C-x C-f") 'helm-find-files)
 
-;; ivy
 (global-set-key (kbd "M-x") 'counsel-M-x)
 (global-set-key (kbd "M-y") 'counsel-yank-pop)
 (global-set-key (kbd "C-x b") 'counsel-switch-buffer)
 (global-set-key (kbd "C-x C-f") 'counsel-find-file)
 
-;; zoom-window
 (global-set-key (kbd "C-x 1") 'zoom-window-zoom)
 
-;; neotree
 (global-set-key [f8] 'neotree-toggle)
 
-;; org-mode
 (setq my/org-mode-prefix-key "C-c o ")
 (global-set-key (kbd (concat my/org-mode-prefix-key "a")) 'org-agenda)
 (global-set-key (kbd (concat my/org-mode-prefix-key "c")) 'org-capture)
 (global-set-key (kbd (concat my/org-mode-prefix-key "l")) 'org-store-link)
 
-;; with keychord
 (key-chord-define-global "jk" 'pretty-hydra-usefull-commands/body)
 
 ;; Don't ask yes or no.
