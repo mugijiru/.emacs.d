1810dc211b4194dcaa340be338b69c91c44f0f3b
mugijiru <106833+mugijiru@users.noreply.github.com>
2021-08-15 21:28:33 +0900

    設定ファイルに反映

---
+++ inits/41-react.el
@@ -0,0 +1,16 @@
+(el-get-bundle dap-mode)
+
+(el-get-bundle web-mode)
+
+(add-to-list 'auto-mode-alist '("\\.[jt]sx" . web-mode))
+
+(defun my/web-mode-tsx-hook ()
+  (let ((ext (file-name-extension buffer-file-name)))
+    (when (or (string-equal "jsx" ext) (string-equal "tsx" ext))
+      (setq web-mode-markup-indent-offset 2)
+      (setq web-mode-code-indent-offset 2)
+      (display-line-numbers-mode t)
+      (lsp)
+      (lsp-ui-mode 1))))
+
+(add-hook 'web-mode-hook 'my/web-mode-tsx-hook)
