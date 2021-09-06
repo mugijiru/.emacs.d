1b4e67eb98c96776661c70ad972693d15ad609d4
mugijiru <106833+mugijiru@users.noreply.github.com>
2021-09-06 10:58:11 +0900

    refile-target の projects 設定を Headline 変更に伴い変更
    タスク管理などに使ってる org-mode のファイルで
    不要に Headline が深くなっていることに気付いて
    先日、1つ不要な Headline を取り除いておいた
    
    それに伴い org-refile-target の設定も
    変更せんとならないことに気付いたので
    projects の target の深さを1つ浅くした

--- inits/61-org-refile.el
+++ inits/61-org-refile.el
@@ -2,7 +2,7 @@
 
 (setq org-refile-use-outline-path 'file)
 
-(setq org-refile-targets `((,(concat org-directory "tasks/projects.org") :level . 2)
+(setq org-refile-targets `((,(concat org-directory "tasks/projects.org") :level . 1)
                            (,(concat org-directory "tasks/pointers.org") :level . 1)
                            (,(concat org-directory "work/scrum/impediments.org") :level . 3)
                            (,(concat org-directory "tasks/next-actions.org") :regexp . "today")
