9905386de8bec6aea0351dd09b12f9c2c9452acf
mugijiru <106833+mugijiru@users.noreply.github.com>
2021-09-18 16:40:29 +0900

    Arrow Function と相性が悪いので smartparens-strict-mode はやめた
    strict だと困ったことになるので
    smartparens-mode を動かすように変更している。

--- inits/41-react.el
+++ inits/41-react.el
@@ -9,7 +9,7 @@
     (when (or (string-equal "jsx" ext) (string-equal "tsx" ext))
       (setq web-mode-markup-indent-offset 2)
       (setq web-mode-code-indent-offset 2)
-      (turn-on-smartparens-strict-mode)
+      (turn-on-smartparens-mode)
       (display-line-numbers-mode t)
       (lsp)
       (lsp-ui-mode 1))))
