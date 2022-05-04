4dc3f89a233e0c39cb391447e8d918a0a1d5bf1d
mugijiru <106833+mugijiru@users.noreply.github.com>
2022-05-04 14:22:12 +0900

    appt-disp-window-function で alert.el が使われるようにした

--- inits/61-org-gcal.el
+++ inits/61-org-gcal.el
@@ -5,3 +5,13 @@
 (require 'org-gcal)
 
 (my/load-config "my-org-gcal-config")
+
+(setq appt-display-format 'window)
+
+(defun my/appt-alert (min-to-app _new-time msg)
+  (interactive)
+  (let ((title (format "あと %s 分" min-to-app)))
+    (message title)
+    (alert msg :title title)))
+
+(setq appt-disp-window-function 'my/appt-alert)
