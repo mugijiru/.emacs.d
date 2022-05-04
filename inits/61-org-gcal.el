c851f19573d12ea4f500392877aad87b43ccc275
mugijiru <106833+mugijiru@users.noreply.github.com>
2022-05-04 14:46:24 +0900

    remove debug code

--- inits/61-org-gcal.el
+++ inits/61-org-gcal.el
@@ -11,7 +11,6 @@
 (defun my/appt-alert (min-to-app _new-time msg)
   (interactive)
   (let ((title (format "あと %s 分" min-to-app)))
-    (message title)
     (alert msg :title title)))
 
 (setq appt-disp-window-function 'my/appt-alert)
