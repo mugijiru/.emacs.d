a768f34ca16272e259deefb45f46acc4c5b29169
mugijiru <106833+mugijiru@users.noreply.github.com>
2022-06-11 20:03:03 +0900

    Update document

--- inits/83-ivy-kibela.el
+++ inits/83-ivy-kibela.el
@@ -1,11 +1,11 @@
 (el-get-bundle ivy-kibela)
 
+(with-eval-after-load 'ivy
+  (require 'ivy-kibela))
+
 (custom-set-variables
  '(ivy-kibela-team (plist-get (nth 0 (auth-source-search :host "kibe.la")) :team))
  '(ivy-kibela-access-token (funcall (plist-get (nth 0 (auth-source-search :host "kibe.la" :max 1)) :secret))))
-
-(with-eval-after-load 'ivy
-  (require 'ivy-kibela))
 
 (with-eval-after-load 'ivy-kibela
   (add-to-list 'ivy-re-builders-alist '(ivy-kibela . ivy-migemo--regex-plus) t))
