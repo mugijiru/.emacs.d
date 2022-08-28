a3f85ba9e1e281fe49aebe6bbf0cc9261e50253a
mugijiru <106833+mugijiru@users.noreply.github.com>
2022-08-28 10:54:39 +0900

    Add create package update pr workflow
    ちょっとお試し実装

--- init-el-get.el
+++ init-el-get.el
@@ -1,4 +1,4 @@
-(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+(add-to-list 'load-path (concat user-emacs-directory "el-get/el-get"))
 
 (unless (require 'el-get nil 'noerror)
   (with-current-buffer
@@ -7,9 +7,31 @@
     (goto-char (point-max))
     (eval-print-last-sexp)))
 
-(add-to-list 'el-get-recipe-path "~/.emacs.d/recipes")
+(add-to-list 'el-get-recipe-path (concat user-emacs-directory "recipes") )
 
 ;; el-get のバージョンロック機構の導入
 (el-get-bundle tarao/el-get-lock)
 (require 'cl)
 (el-get-lock)
+
+(defun my/el-get-auto-update (package)
+  (let ((el-get-default-process-sync t)
+        (old-checksum (my/el-get-lock-checksum package)))
+    (el-get-lock-checkout "el-get")
+    (sit-for 10) ;; el-get がなんか読み込まれてるので待ってみる
+    (el-get-lock-checkout package)
+    (el-get-update package)
+    (let ((new-checksum (my/el-get-lock-checksum package)))
+      (unless (string= old-checksum new-checksum)
+        (let* ((compare (concat old-checksum "..." new-checksum))
+               (title (concat "Update " package))
+               (recipe (ignore-errors (el-get-package-def package)))
+               (pkgname (plist-get recipe :pkgname))
+               (body (concat "https://github.com/" pkgname "/compare/" compare))
+               (commit-message (concat title "\n\n" body)))
+          (write-region commit-message nil "/tmp/commit-message.txt"))))))
+
+(defun my/el-get-lock-checksum (package)
+  (let ((versions (cdr el-get-lock-package-versions))
+        (package-sym (intern package)))
+    (cadr (alist-get package-sym (cdr el-get-lock-package-versions)))))
