1ead6c10635fbd3008a326e8438105467d46ae27
mugijiru <106833+mugijiru@users.noreply.github.com>
2022-09-25 21:01:02 +0900

    todoist の token を authinfo から読むようにした

--- inits/50-todoist.el
+++ inits/50-todoist.el
@@ -1,4 +1,4 @@
 (el-get-bundle emacs-todoist)
 
-(with-eval-after-load 'org
-  (my/load-config "my-todoist-config"))
+(with-eval-after-load 'todoist
+  (setq todoist-token (funcall (plist-get (nth 0 (auth-source-search :host "todoist.com" :max 1)) :secret))))
