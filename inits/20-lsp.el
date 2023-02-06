f5d2cb0671c448adf03d7f7f80539f4821a3a6a7
mugijiru <106833+mugijiru@users.noreply.github.com>
2023-02-06 23:54:23 +0900

    lsp-mode 用に pretty-hydra を設定した

--- inits/20-lsp.el
+++ inits/20-lsp.el
@@ -17,5 +17,22 @@
           'width))
   (defun lsp-ui-sideline--compute-height () nil))
 
+(with-eval-after-load 'pretty-hydra
+  (pretty-hydra-define pretty-hydra-lsp (:separator "-" :color blue :foreign-keys warn :title "LSP" :quit-key "q")
+    ("Find"
+     (("x" lsp-find-definition "definition")
+      ("r" lsp-find-references "references")
+      ("t" lsp-find-type-definition "type")
+      ("i" lsp-find-implementation "implementation")
+      ("D" lsp-find-declaration "declaration"))
+
+     "Code"
+     (("m" lsp-rename "Rename"))
+
+     "UI"
+     (("I" lsp-ui-imenu "imenu")
+      ("X" lsp-ui-peek-find-definitions "def")
+      ("R" lsp-ui-peek-find-references "refs")))))
+
 (with-eval-after-load 'lsp-mode
   (add-to-list 'lsp--formatting-indent-alist `(web-mode . web-mode-code-indent-offset)))
