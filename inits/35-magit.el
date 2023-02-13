087c06351b8af83edfa8a85cd7cc652d55cd5972
mugijiru <106833+mugijiru@users.noreply.github.com>
2023-02-13 12:50:09 +0900

    Add orgit
    https://github.com/magit/orgit
    便利そうなので入れてみた

--- inits/35-magit.el
+++ inits/35-magit.el
@@ -1,3 +1,5 @@
 (el-get-bundle magit)
 (with-eval-after-load 'magit
-                      (add-to-list 'load-path (expand-file-name "~/.emacs.d/el-get/ghub/lisp")))
+  (add-to-list 'load-path (expand-file-name "~/.emacs.d/el-get/ghub/lisp")))
+
+(el-get-bundle orgit)
