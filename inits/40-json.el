7d565282a65e761210baf188a66b58950cbcd6d6
mugijiru <106833+mugijiru@users.noreply.github.com>
2023-02-20 09:04:33 +0900

    json-mode 用に設定を追加
    - lsp-mode で現在位置の key を header に表示させるようにした
    - smartparens-strict-mode の利用。これでカッコが崩れない
    - flycheck で json-jq を使うようにした。lsp では微妙だったので
    - highlight-indent-guides-mode を使う。どこにいるかわからんので
    - 行数も表示するようにした

--- inits/40-json.el
+++ inits/40-json.el
@@ -1,1 +1,14 @@
 (el-get-bundle json-mode)
+
+(defun my/json-mode-hook ()
+  (company-mode 1)
+  (lsp)
+  (lsp-ui-mode 1)
+  (add-hook 'before-save-hook #'lsp-format-buffer nil 'local)
+  (turn-on-smartparens-strict-mode)
+  (flycheck-mode 1)
+  (flycheck-select-checker 'json-jq)
+  (highlight-indent-guides-mode 1)
+  (display-line-numbers-mode 1))
+
+(add-hook 'json-mode-hook 'my/json-mode-hook)
