19886626329d3ad2224861f2859bc0e4b6977106
mugijiru <106833+mugijiru@users.noreply.github.com>
2023-02-21 23:00:41 +0900

    enh-ruby-mode の時の before-save-hook の設定を変更
    local 変数にしておいた

--- inits/40-ruby.el
+++ inits/40-ruby.el
@@ -14,7 +14,7 @@
   (company-mode 1)
   (lsp)
   (lsp-ui-mode 1)
-  (add-hook 'before-save-hook #'lsp-format-buffer)
+  (add-hook 'before-save-hook #'lsp-format-buffer nil 'local)
   (turn-on-smartparens-strict-mode)
   (display-line-numbers-mode 1))
 
