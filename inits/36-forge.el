536bd5a425737d0c437d4ef21273f72c85a6351f
mugijiru <106833+mugijiru@users.noreply.github.com>
2023-02-21 22:22:20 +0900

    forge-post-mode 起動時に flycheck を有効にする
    PR 作る時に textlint でチェックしてほしいので

--- inits/36-forge.el
+++ inits/36-forge.el
@@ -2,3 +2,9 @@
 
 (with-eval-after-load 'magit
   (require 'forge))
+
+(defun my/forge-post-mode-hook ()
+  (flycheck-mode 1))
+
+(with-eval-after-load 'forge
+  (add-hook 'forge-post-mode-hook 'my/forge-post-mode-hook))
