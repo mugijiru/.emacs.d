e3d4651e44f100540e47709060acb2ac9108a901
mugijiru <106833+mugijiru@users.noreply.github.com>
2023-04-13 14:07:15 +0900

    Add copilot and editorconfig recipe
    copilot.el を入れるためにレシピを追加した。
    editorconfig は依存関係に含まれるため合わせて追加している。

---
+++ recipes/copilot.el
@@ -0,0 +1,7 @@
+(:name copilot
+       :website "https://github.com/zerolfx/copilot.el"
+       :description "An Emacs plugin for GitHub Copilot."
+       :type github
+       :branch "main"
+       :pkgname "zerolfx/copilot.el"
+       :depends (s dash editorconfig json-rpc))
