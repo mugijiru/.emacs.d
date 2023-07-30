ab9b9e569d988d81ea1a8396371b8c63361f1446
mugijiru <106833+mugijiru@users.noreply.github.com>
2023-07-30 14:29:47 +0900

    fix company-quickhelp setting
    有効にする方法を間違えていたので修正

--- inits/30-company.el
+++ inits/30-company.el
@@ -7,10 +7,10 @@
 (custom-set-variables '(company-show-quick-access t))
 
 (el-get-bundle company-quickhelp)
+(company-quickhelp-mode t)
 
 (custom-set-variables
- '(company-quickhelp-color-background "#323445")
- '(company-quickhelp-mode t))
+ '(company-quickhelp-color-background "#323445"))
 
 (el-get-bundle company-posframe)
 (company-posframe-mode 1)
