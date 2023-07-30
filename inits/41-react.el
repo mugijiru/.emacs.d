26da5091d40520f3086706d6121272d48fdd3e95
mugijiru <106833+mugijiru@users.noreply.github.com>
2023-07-30 20:40:07 +0900

    Enable origami on tsx

--- inits/41-react.el
+++ inits/41-react.el
@@ -18,6 +18,7 @@
       (setq web-mode-markup-indent-offset 2)
       (setq web-mode-code-indent-offset 2)
       (setq web-mode-enable-auto-indentation nil)
+      (origami-mode 1)
       (company-mode 1)
       (turn-on-smartparens-mode)
       (display-line-numbers-mode t)
