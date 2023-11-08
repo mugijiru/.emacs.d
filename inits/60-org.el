e5ca4b7bbf54bb81c5fdd268f457d6efa4453752
mugijiru <106833+mugijiru@users.noreply.github.com>
2023-11-08 22:01:44 +0900

    refactor: extract org-mode-hook
    関数に分離しないと再設定時に扱いづらいので分離した

--- inits/60-org.el
+++ inits/60-org.el
@@ -62,15 +62,16 @@
     (add-hook 'org-shiftdown-final-hook 'windmove-down)
     (add-hook 'org-shiftright-final-hook 'windmove-right)))
 
-(add-hook 'org-mode-hook
-          (lambda ()
-            (my/org-mode-map-override-windmove-mode-map)
+(defun my/org-mode-hook ()
+  (my/org-mode-map-override-windmove-mode-map)
 
-            ;; http://yitang.uk/2022/07/05/move-between-window-using-builtin-package/
-            (define-key org-read-date-minibuffer-local-map (kbd "<left>") (lambda () (interactive) (org-eval-in-calendar '(calendar-backward-day 1))))
-            (define-key org-read-date-minibuffer-local-map (kbd "<right>") (lambda () (interactive) (org-eval-in-calendar '(calendar-forward-day 1))))
-            (define-key org-read-date-minibuffer-local-map (kbd "<up>") (lambda () (interactive) (org-eval-in-calendar '(calendar-backward-week 1))))
-            (define-key org-read-date-minibuffer-local-map (kbd "<down>") (lambda () (interactive) (org-eval-in-calendar '(calendar-forward-week 1))))))
+  ;; http://yitang.uk/2022/07/05/move-between-window-using-builtin-package/
+  (define-key org-read-date-minibuffer-local-map (kbd "<left>") (lambda () (interactive) (org-eval-in-calendar '(calendar-backward-day 1))))
+  (define-key org-read-date-minibuffer-local-map (kbd "<right>") (lambda () (interactive) (org-eval-in-calendar '(calendar-forward-day 1))))
+  (define-key org-read-date-minibuffer-local-map (kbd "<up>") (lambda () (interactive) (org-eval-in-calendar '(calendar-backward-week 1))))
+  (define-key org-read-date-minibuffer-local-map (kbd "<down>") (lambda () (interactive) (org-eval-in-calendar '(calendar-forward-week 1)))))
+
+(add-hook 'org-mode-hook 'my/org-mode-hook)
 
 (with-eval-after-load 'org-mode
   (my/org-mode-map-override-windmove-mode-map))
