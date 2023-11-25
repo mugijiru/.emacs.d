f5371a1d10d558140e83b61f8f74c3b888b57ddc
mugijiru <106833+mugijiru@users.noreply.github.com>
2023-11-25 22:40:59 +0900

    fix: Rename next-actoins.org to habits.org
    もう next-actions.org では habits しか使ってないので
    そっちのファイル名を habits.org に変更した。
    
    それに合わせて設定ファイルも変更している

--- inits/61-org-refile.el
+++ inits/61-org-refile.el
@@ -6,8 +6,6 @@
                            (,(concat org-directory "tasks/projects.org") :level . 1)
                            (,(concat org-directory "tasks/pointers.org") :level . 1)
                            (,(concat org-directory "work/scrum/impediments.org") :level . 3)
-                           (,(concat org-directory "tasks/next-actions.org") :regexp . "today")
-                           (,(concat org-directory "tasks/next-actions.org") :regexp . "C-")
                            (,(concat org-directory "private/2020_summary.org") :level . 2)
                            (,(concat org-directory "tasks/shopping.org") :level . 1)
                            (,(concat org-directory "tasks/someday.org") :level . 1)))
