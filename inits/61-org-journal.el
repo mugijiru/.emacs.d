55d29e0647ae857d1cb8b861f31697ded34654b0
mugijiru <106833+mugijiru@users.noreply.github.com>
2024-03-24 10:47:24 +0900

    fix: Disable org-journal to agenda integration
    org-journal の保存時に
    org-agenda-targets を自動で更新する機能を無効にした。
    
    これを有効にしていると保存時に時間がかかるので、
    一旦無効にして運用してみる。

--- inits/61-org-journal.el
+++ inits/61-org-journal.el
@@ -15,7 +15,7 @@
  '(org-journal-dir (concat org-directory "journal/"))
  '(org-journal-file-format "%Y%m%d.org")
  '(org-journal-date-format "%d日(%a)")
- '(org-journal-enable-agenda-integration t)
+ '(org-journal-enable-agenda-integration nil)
  '(org-journal-carryover-items "TODO={TODO\\|DOING\\|WAIT}"))
 
 (with-eval-after-load 'org-journal
