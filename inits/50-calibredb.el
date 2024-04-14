eaa3410ae4b3387311206e1574591cddd0b33ff4
mugijiru <106833+mugijiru@users.noreply.github.com>
2024-04-14 11:18:21 +0900

    feat: Add calibredb-filter hydra
    calibredb でのフィルタ処理用の Hydra を追加した

--- inits/50-calibredb.el
+++ inits/50-calibredb.el
@@ -2,3 +2,18 @@
 
 (custom-set-variables
  '(calibredb-root-dir "~/calibre-library"))
+
+(with-eval-after-load 'pretty-hydra
+  (pretty-hydra-define calibredb-filter-hydra (:separator "-" :exit t :quit-key "q" :title (concat (all-the-icons-material "filter_list") " calibredb filter"))
+    ("by"
+     (("t" calibredb-filter-by-tag           "Tag")
+      ("a" calibredb-filter-by-author-sort   "Author")
+      ("f" calibredb-filter-by-book-format   "Format")
+      ("d" calibredb-filter-by-last_modified "Updated at")
+      ("v" calibredb-virtual-library-list    "Virtual Library")
+      ("l" calibredb-library-list            "Library"))
+
+    "Other"
+    (("R" calibredb-search-clear-filter "Reset"))))
+
+  )
