b711e679b8d08bf07d7ed257396f5b94f319e71d
mugijiru <106833+mugijiru@users.noreply.github.com>
2024-12-12 15:53:35 +0900

    fix: setopt copilot-chat-prompt after load copilot-chat-common
    copilot-chat-prompot は
    copilot-chat-common が読まれた後でないと設定できなかったので修正

--- inits/30-copilot.el
+++ inits/30-copilot.el
@@ -21,8 +21,9 @@
 
 (setopt copilot-chat-frontend 'shell-maker)
 
-(setq my/copilot-chat-prompt-original copilot-chat-prompt)
-(setopt copilot-chat-prompt (concat my/copilot-chat-prompt-original "\n出力には日本語を用います"))
+(with-eval-after-load 'copilot-chat-common
+  (setq my/copilot-chat-prompt-original copilot-chat-prompt)
+  (setopt copilot-chat-prompt (concat my/copilot-chat-prompt-original "\n出力には日本語を用います")))
 
 (with-eval-after-load 'pretty-hydra
   (pretty-hydra-define copilot-chat-hydra
