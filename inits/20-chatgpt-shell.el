45c3e0c9255e0285cf09fda341c5337bd63a1a44
mugijiru <106833+mugijiru@users.noreply.github.com>
2025-02-07 22:46:34 +0900

    chore: Bump chatgpt-shell model version to gemini-2.0-flash-latest

--- inits/20-chatgpt-shell.el
+++ inits/20-chatgpt-shell.el
@@ -1,5 +1,5 @@
 (el-get-bundle chatgpt-shell)
 
-(setopt chatgpt-shell-model-version "gemini-1.5-flash-latest")
+(setopt chatgpt-shell-model-version "gemini-2.0-flash-latest")
 (setopt chatgpt-shell-google-key
         (funcall (plist-get (nth 0 (auth-source-search :host "gemini" :max 1)) :secret)))
