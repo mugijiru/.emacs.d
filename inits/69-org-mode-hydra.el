e5687efa8ce551be93ce31500ee9b9dece7eddbc
mugijiru <106833+mugijiru@users.noreply.github.com>
2025-04-22 09:15:50 +0900

    chore: Align

--- inits/69-org-mode-hydra.el
+++ inits/69-org-mode-hydra.el
@@ -19,8 +19,8 @@
 
      "View"
      (("N" org-toggle-narrow-to-subtree "Toggle Subtree")
-      ("C" org-columns "Columns")
-      ("O" org-global-cycle "Toggle open")
+      ("C" org-columns                  "Columns")
+      ("O" org-global-cycle             "Toggle open")
       ("D" my/org-clock-toggle-display  "Toggle Display"))
 
      "Task"
