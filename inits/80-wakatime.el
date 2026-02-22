f63310d0a8fc8b60f98146724dedd78d31fdb4c2
mugijiru <106833+mugijiru@users.noreply.github.com>
2026-02-22 14:25:21 +0900

    docs(wakatime): comment out global-wakatime-mode for temporary disable
    - wakatime-mode の自動有効化を一時的に無効化した
    - init.org, inits/80-wakatime.el, ドキュメントを対応して修正
    - 必要に応じてコメントを外して再有効化可能

--- inits/80-wakatime.el
+++ inits/80-wakatime.el
@@ -4,4 +4,4 @@
  '(wakatime-cli-path "/usr/bin/wakatime")
  '(wakatime-api-key (funcall (plist-get (nth 0 (auth-source-search :host "wakatime.com" :max 1)) :secret))))
 
-(global-wakatime-mode 1)
+;; (global-wakatime-mode 1)
